eb6e53379e2e3952751a6b6d85407cbeafec82a9
Brad Wilcox <brad_wilcox@me.com>
2013-11-06 10:05:20 -0500

    creating controller, view and adding root for home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ RailsBootstrap::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root :to => 'visitors#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
