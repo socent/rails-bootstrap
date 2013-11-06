5bae95a1e0cd5d546024e1fdb7d684e6a95ff2bd
Brad Wilcox <brad_wilcox@me.com>
2013-11-06 10:38:52 -0500

    adding bootstrap directive to js manifest file; creating bootstrap overrides Sass file and importing bootstrap; renaming application css file to Sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,5 +1,5 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
+// Manifest file that'll be compiled into application.js, 
+// which will include all the concatenated files listed below.
 //
 // Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
 // or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap
 //= require_tree .
